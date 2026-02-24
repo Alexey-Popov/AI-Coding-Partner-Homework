a78f8eeba665c01e7c6268f9f12aaf1f06804b85
Ruslan Pistriak <ruslan.pistriak@pitchbook.com>
2026-02-24 09:34:55 +0200

    remove obsolete dependencies

--- homework-2/src/main/java/com/css/config/JacksonConfig.java
+++ homework-2/src/main/java/com/css/config/JacksonConfig.java
@@ -5,10 +5,18 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class JacksonConfig {
+public class JacksonConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
 
     @Bean
     public ObjectMapper objectMapper() {
